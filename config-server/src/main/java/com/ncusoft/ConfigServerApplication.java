6239064e1b51227534df504c9506d8199a915496
jie12366 <2263509062@qq.com>
2019-10-07 13:37:18 +0800

    :art: 修改配置文件

--- config-server/src/main/java/com/ncusoft/ConfigServerApplication.java
+++ config-server/src/main/java/com/ncusoft/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.ncusoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author monkJay
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
